ca88375c896a59f1269235f1949b6aaff5d5101a
Chuck Lightfoot <charleslightfootjr@gmail.com>
2021-05-06 15:04:28 -0500

    Added Directives
    Directives add to course project.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,8 +11,14 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes:Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.pressurecookrecipes.com/wp-content/uploads/2017/01/instant-pot-pork-roast.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.pressurecookrecipes.com/wp-content/uploads/2017/01/instant-pot-pork-roast.jpg'),
+    new Recipe('A Test Recipe',
+    'This is simply a test',
+    'https://www.pressurecookrecipes.com/wp-content/uploads/2017/01/instant-pot-pork-roast.jpg'),
+
+
+    new Recipe('Luv Jerky',
+    'The best jerky in the world!',
+    'http://www.mutherfudger.co.uk/wp-content/uploads/2017/08/beef-jerky.jpg'),
 
   ];
 
